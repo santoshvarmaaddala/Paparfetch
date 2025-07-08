e788a7588572ef0b61a84b894b583b78b7a87fa8
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-07-08 16:46:25 +0530

    comments added for readability

--- get_papers_list.py
+++ get_papers_list.py
@@ -2,26 +2,29 @@ import argparse
 import csv
 from src.get_papers.main import fetch_paper_details, fetch_pubmed_ids
 
+# entry point for cmd line tool
 def main():
     parser = argparse.ArgumentParser(description="Fetch PubMed papers for pharma authors.")
+
+    # arguments or flags 
     parser.add_argument("query", help="Search query for PubMed")
     parser.add_argument("-d", "--debug", action="store_true")
     parser.add_argument("-f", "--file", help="Output CSV filename")
 
     args = parser.parse_args()
 
-    ids = fetch_pubmed_ids(args.query)
+    ids = fetch_pubmed_ids(args.query) # extracted pubmedids
     if args.debug:
         print(f"Found PubMed IDs: {ids}")
 
-    results = fetch_paper_details(ids)
+    results = fetch_paper_details(ids) # extracted papaer details
     if args.file:
-        with open(args.file, "w", newline="", encoding="utf-8") as f:
+        with open(args.file, "w", newline="", encoding="utf-8") as f: # writing results into csv file
             writer = csv.DictWriter(f, fieldnames=results[0].keys())
             writer.writeheader()
             writer.writerows(results)
     else:
-        for row in results:
+        for row in results: 
             print(row)
 
 if __name__ == '__main__':
--- src/get_papers/main.py
+++ src/get_papers/main.py
@@ -2,10 +2,11 @@ import requests
 import xml.etree.ElementTree as ET
 from typing import List, Tuple, Dict
 
+# function responsible for fetching pubmed ids
 def fetch_pubmed_ids(query: str) -> List[str]:
     url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     params = {
-        "db": "pubmed",
+        "db": "pubmed", # database name
         "term": query,
         "retmax": 20
     }
@@ -13,6 +14,7 @@ def fetch_pubmed_ids(query: str) -> List[str]:
     tree = ET.fromstring(res.text)
     return [id_elem.text for id_elem in tree.findall(".//Id")]
 
+# function responsiable for fetching paper details based on pubmedids
 def fetch_paper_details(pubmub_ids: List[str]) -> List[Dict]:
     url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
     params = {
@@ -23,7 +25,7 @@ def fetch_paper_details(pubmub_ids: List[str]) -> List[Dict]:
     res = requests.get(url, params=params)
     root = ET.fromstring(res.text)
 
-    results = []
+    results = [] # list to store results
     for article in root.findall(".//PubmedArticle"):
         pmid = article.findtext(".//PMID")
         title = article.findtext(".//ArticleTitle", default="No Title")
@@ -35,15 +37,16 @@ def fetch_paper_details(pubmub_ids: List[str]) -> List[Dict]:
 
         for affiliation in affiliations:
             affiliation_text = affiliation.findtext("Affiliation", default="").lower()
-            if not any(x in affiliation_text for x in ["university", "college", "research", "school", "institute"]):
+            if not any(x in affiliation_text for x in ["university", "college", "research", "school", "institute"]): # filtering the article by affiliation
                 if any(x in affiliation_text for x in ["pharma", "biotech", "inc", "ltd", "corp"]):
                     company_names.append(affiliation_text)
                     non_academic_authors.append(affiliation_text)
 
         email = None
         if "@" in str(affiliations):
-            email = extract_email(str(affiliations))
+            email = extract_email(str(affiliations)) # extracting email
 
+        # adding result
         results.append({
             "PubmedID": pmid,
             "Title": title,
@@ -51,11 +54,12 @@ def fetch_paper_details(pubmub_ids: List[str]) -> List[Dict]:
             "Non-academic Author(s)": "; ".join(set(non_academic_authors)),
             "Company Affiliation(s)": "; ".join(set(company_names)),
             "Corresponding Author Email": email or "N/A"
-        })
+        }) 
 
     return results
 
+# function for email extraction
 def extract_email(text: str) -> str:
     import re
-    match = re.search(r"[\w\.-]+@[\w\.-]+", text)
+    match = re.search(r"[\w\.-]+@[\w\.-]+", text) # email regular expression to match string
     return match.group(0) if match else "N/A"
